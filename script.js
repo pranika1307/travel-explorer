3154cff8eab7177ccbe7bfafb51de41f751beb09
Pranika seth <sethpakhi20@gmail.com>
2025-09-27 21:25:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const UNSPLASH_ACCESS_KEY = "YOUR_UNSPLASH_ACCESS_KEY"; // Replace with your Unsplash API key
-const WEATHER_API_KEY = "YOUR_OPENWEATHERMAP_API_KEY"; // Replace with your OpenWeatherMap API key
+const UNSPLASH_ACCESS_KEY = "M4xC0vA7pKIMtuJEi3RAT1EMmb74IhRys6wNvOn9ZN4"; // Replace with your Unsplash API key
+const WEATHER_API_KEY = "9c8cf55024bb1e933869b6664bc0bdb4"; // Replace with your OpenWeatherMap API key
 
 async function exploreDestination() {
   const query = document.getElementById("destination").value;
